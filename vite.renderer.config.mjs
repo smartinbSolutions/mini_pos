9dedc95dc0b45332efce65885e223175d559af1b
DevZeroSh <m.shahrour.work@gmail.com>
2026-05-18 09:21:32 +0300

    fixed bugs

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig, transformWithEsbuild } from "vite";
-import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config
 export default defineConfig({
@@ -16,7 +15,6 @@ export default defineConfig({
         });
       },
     },
-    react(),
   ],
   esbuild: {
     loader: "jsx",
