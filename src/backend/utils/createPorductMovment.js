bc7188f81d2ff0cc15e0c28a227e6cd479a58a13
DevZeroSh <m.shahrour.work@gmail.com>
2026-07-02 15:34:47 +0300

    feat: add createProductMovement function for handling product movements in the database

---
+++ src/backend/utils/createPorductMovment.js
@@ -0,0 +1,35 @@
+export default function createProductMovement(db, data) {
+  const stmt = db.prepare(`
+    INSERT INTO product_movements (
+      product_id,
+      reference_id,
+      reference_type,
+      type,
+      action,
+      enterPrice,
+      outPrice,
+      quantity
+    )
+    VALUES (
+      @product_id,
+      @reference_id,
+      @reference_type,
+      @type,
+      @action,
+      @enterPrice,
+      @outPrice,
+      @quantity
+    )
+  `);
+
+  return stmt.run({
+    product_id: data.product_id,
+    reference_id: data.reference_id ?? null,
+    reference_type: data.reference_type ?? null,
+    type: data.type,
+    action: data.action,
+    enterPrice: Number(data.enterPrice || 0),
+    outPrice: Number(data.outPrice || 0),
+    quantity: Number(data.quantity || 0),
+  });
+}
