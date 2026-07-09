68f43045813ce09eca3ef8e2730be6307aceb679
DevZeroSh <m.shahrour.work@gmail.com>
2026-07-09 11:01:45 +0300

    refactor: remove unused reverse payment services and streamline payment handling

--- src/backend/services/payment/invoice/reversePayment.service.js
+++ src/backend/services/payment/invoice/reversePayment.service.js
@@ -1,14 +1,6 @@
 export default function reversePayment(db, payment) {
   db.prepare(
     `
-    UPDATE funds
-    SET balance = balance - ?
-    WHERE id = ?
-    `,
-  ).run(Number(payment.amount_fund_currency), payment.fund_id);
-
-  db.prepare(
-    `
     DELETE FROM payment_allocations
     WHERE payment_id = ?
       AND invoice_type = 'sales'
